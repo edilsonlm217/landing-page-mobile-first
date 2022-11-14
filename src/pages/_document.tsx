d5d2c9b0e975ae017c6f809e7f798eb255935cec
Edilson Lima <edilson.lima.3@globalhitss.com.br>
2022-11-14 00:27:55 -0400

    Adição de [;] no final da primeira linha de import

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, { DocumentContext, DocumentInitialProps, Html, Head, Main, NextScript } from 'next/document'
+import Document, { DocumentContext, DocumentInitialProps, Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
