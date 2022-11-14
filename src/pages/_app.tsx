8148d55ab6ec4b9c3129e4b94540b7896c9873cf
Edilson Lima <edilson.lima.3@globalhitss.com.br>
2022-11-14 01:26:47 -0400

    Implementação do grid da aplicação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
 import '../styles/global.scss';
 import type { AppProps } from 'next/app';
 
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+import styles from '../styles/app.styles.module.scss';
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <main>
+        <Component {...pageProps} />
+      </main>
+      <Footer />
+    </>
+  );
 }
