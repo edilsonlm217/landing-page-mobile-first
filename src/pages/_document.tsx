cf7998d5c4af8fd512cf8f00b92fd7d854a6e52a
Edilson Lima <edilson.lima.3@globalhitss.com.br>
2022-11-13 22:07:01 -0400

    Adição de fonte e script GA

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,25 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" />
+        </Head>
+
+        {/* <!-- Google tag (gtag.js) --> */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-Q9PS77NP82"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-Q9PS77NP82');
+          `
+        }}>
+        </script>
+
         <body>
           <Main />
           <NextScript />
